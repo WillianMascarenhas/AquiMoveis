32a9a392f503dae4828b1cfd354e8ec9824c41ec
WillianMascarenhas <masca.will@gmail.com>
2023-08-31 14:58:00 -0300

    feat: folder architecture to initialize the server

---
+++ Back/src/app.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+
+export default app;
