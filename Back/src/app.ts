dc8c702232df29341c76f306e49db08de38369b2
WillianMascarenhas <masca.will@gmail.com>
2023-09-01 13:17:46 -0300

    feat: handle errors

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -2,12 +2,17 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import cors from "cors"
+import "dotenv/config"
+import { handlerAppError } from "./errors/handleAppErro";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: process.env.URL_FROM_FRONT
 }))
+
+
+app.use(handlerAppError)
 
 export default app;
