9e21c99046f1f772cc2e0ce29fe95d8b374b60b0
WillianMascarenhas <masca.will@gmail.com>
2023-09-20 15:15:26 -0300

    feat: main base

---
+++ Front/src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="flex justify-center items-center w-screen h-[450px] bg-brand2-750 p-[100px-0px]">
+
+        </footer>
+    )
+}
