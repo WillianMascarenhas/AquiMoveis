68b2c00ee639fb616e2be462c46047fc6f18ea55
WillianMascarenhas <masca.will@gmail.com>
2023-09-21 21:57:36 -0300

    feat: change server port

--- Back/src/server.ts
+++ Back/src/server.ts
@@ -4,10 +4,10 @@ import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize()
     .then(() => {
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT || 3001;
         app.listen(PORT, () => {
-          console.log("Server is running on port 3000");
-          console.log("'baseUrl': 'http://127.0.0.1:3000'");
+          console.log("Server is running on port 3001");
+          console.log("'baseUrl': 'http://127.0.0.1:3001'");
         });
     })
     .catch((error) =>{console.log(error)})
