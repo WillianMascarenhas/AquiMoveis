dbe6e322eda9b579213ca9f76afd343c51dfc822
WillianMascarenhas <masca.will@gmail.com>
2023-09-27 11:08:34 -0300

    feat: type in furniture fix: middleware patch furniture

--- Back/src/middlewares/ensureFurniture.middleware.ts
+++ Back/src/middlewares/ensureFurniture.middleware.ts
@@ -6,18 +6,26 @@ import { AppError } from "../errors/app.error";
 export const ensureFurnitureMiddleware = async (req:Request, res: Response, next: NextFunction) =>{
     const furnitureId = parseInt(req.params.id)
     const furnitureRepository = AppDataSource.getRepository(Furniture)
-
-
+    
+    
     if(req.method === "PATCH"){
-        const findFurnitureByName = await furnitureRepository.findOneBy({name: req.body.name})
         const findFurnitureById = await furnitureRepository.findOneBy({id:furnitureId})
-
-        if(findFurnitureByName?.name === findFurnitureById?.name){
+        
+        if(req.body.name !== undefined){
+            const findFurnitureByName = await furnitureRepository.findOneBy({name: req.body.name})
+            
+            if(findFurnitureByName?.name === findFurnitureById?.name){
+                return next()
+            }else if(findFurnitureByName?.name !== findFurnitureById?.name && findFurnitureByName){
+                throw new AppError("Furniture already registered", 404)
+            }
             return next()
-        }else if(findFurnitureByName?.name !== findFurnitureById?.name && findFurnitureByName){
-            throw new AppError("Furniture already registered", 401)
         }
-        return next()
+        if(findFurnitureById?.name){
+            return next()
+        }
+
+
     }
     
     const findFurniture = await furnitureRepository.findOneBy({name: req.body.name})
