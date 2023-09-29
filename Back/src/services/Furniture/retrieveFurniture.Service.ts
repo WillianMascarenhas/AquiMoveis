a3a43375ffb223d7c361d50d3c407f3d37f65204
WillianMascarenhas <masca.will@gmail.com>
2023-09-29 17:10:06 -0300

    feat: pagination in retrieve furniture

--- Back/src/services/Furniture/retrieveFurniture.Service.ts
+++ Back/src/services/Furniture/retrieveFurniture.Service.ts
@@ -5,23 +5,48 @@ import { furnituresResponseSchema } from "../../schemas/furniture.schema";
 
 export const retrieveFurnitureService = async (queryReq:any):Promise<IFurnituresResponse> =>{
     const furnitureRepository = AppDataSource.getRepository(Furniture)
+    const page= Number(queryReq.page)
+    const perPage= Number(queryReq.perPage)
+
+    let listFurniture: Furniture[] | undefined
+
     if(queryReq.type){
         const query = queryReq.type.toUpperCase()
+        if(page || perPage){
+            listFurniture = await furnitureRepository.find({
+                skip: (page - 1) * perPage,
+                take: perPage,
+                where: {
+                    type: query
+                },
+                relations: ["furnitureImages"]
+            })
+            return furnituresResponseSchema.parse(listFurniture)
+        }else{
+            listFurniture = await furnitureRepository.find({
+                where: {
+                    type: query
+                },
+                relations: ["furnitureImages"]
+            })
+            return furnituresResponseSchema.parse(listFurniture)
+        }
         
-        const listFurniture = await furnitureRepository.find({
-            where: {
-                type: query
-            },
+
+    }
+    if(page || perPage){
+        listFurniture = await furnitureRepository.find({
+            skip: (page - 1) * perPage,
+            take: perPage,
             relations: ["furnitureImages"]
         })
 
-        return furnituresResponseSchema.parse(listFurniture)
+        return  furnituresResponseSchema.parse(listFurniture)
+    }else{
+        listFurniture = await furnitureRepository.find({
+            relations: ["furnitureImages"]
+        })
+
+        return  furnituresResponseSchema.parse(listFurniture)
     }
-
-    const listFurniture = await furnitureRepository.find({
-        relations: ["furnitureImages"]
-    })
-
-    return  furnituresResponseSchema.parse(listFurniture)
-
 }
